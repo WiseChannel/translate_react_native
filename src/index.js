f5a113da1635c03c4658cb802b70ee60d0084cef
Wise Ilya <johndoe@example.com>
2019-03-16 14:34:49 +0200

    moChange

--- src/index.js
+++ src/index.js
@@ -3,9 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+    class listKey extends React.Component() {
+        constructor(props,text) {
+            this.text = text;
+            text = 'gl';
+        }
+
+
+        onLiteFunc() {
+            return(
+                 <h1>{this.text}</h1>
+            )
+        }
+
+        render() {
+            return(
+                <div></div>
+            )
+        }
+    }
+
+
 ReactDOM.render(
     <div className='text'>
-        <h1>123</h1>
+        <h1>{}</h1>
     </div>,
     document.getElementById('root')
 )
