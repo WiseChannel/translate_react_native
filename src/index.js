401e6ba3dd8ad2cf0cbbeb2e955bfa7d15bac85c
Wise Ilya <johndoe@example.com>
2019-03-16 16:36:21 +0200

    new page and file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
 
 
-    class listKey extends React.Component() {
-        constructor(props,text) {
-            this.text = text;
-            text = 'gl';
-        }
+//     class listKey extends React.Component() {
+//         constructor(props,text) {
+//             this.text = text;
+//             text = 'gl';
+//         }
 
 
-        onLiteFunc() {
-            return(
-                 <h1>{this.text}</h1>
-            )
-        }
+//         onLiteFunc() {
+//             return(
+//                  <h1>{this.text}</h1>
+//             )
+//         }
 
-        render() {
-            return(
-                <div></div>
-            )
-        }
-    }
+//         render() {
+//             return(
+//                 <div></div>
+//             )
+//         }
+//     }
 
 
-ReactDOM.render(
-    <div className='text'>
-        <h1>{}</h1>
-    </div>,
-    document.getElementById('root')
-)
+// ReactDOM.render(
+//     <div className='text'>
+//         <h1>{}</h1>
+//     </div>,
+//     document.getElementById('root')
+// )
