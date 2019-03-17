92619c13c3c4213944170f0028e8aa4a2c164c6a
Wise Ilya <johndoe@example.com>
2019-03-17 13:40:59 +0200

    minor

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-
-//     class listKey extends React.Component() {
-//         constructor(props,text) {
-//             this.text = text;
-//             text = 'gl';
-//         }
-
-
-//         onLiteFunc() {
-//             return(
-//                  <h1>{this.text}</h1>
-//             )
-//         }
-
-//         render() {
-//             return(
-//                 <div></div>
-//             )
-//         }
-//     }
 
 
 // ReactDOM.render(
-//     <div className='text'>
-//         <h1>{}</h1>
-//     </div>,
+    
 //     document.getElementById('root')
 // )
