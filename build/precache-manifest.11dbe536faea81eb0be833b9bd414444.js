9620ec3c9a717aec7881221afc2141ea1337a01c
Wise Ilya <johndoe@example.com>
2019-03-24 18:46:21 +0200

    npm bild

---
+++ build/precache-manifest.11dbe536faea81eb0be833b9bd414444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7fe6a12c7050af1fafd6",
+    "url": "/static/js/main.c7c612b0.chunk.js"
+  },
+  {
+    "revision": "e04b92c7128ef3b87d79",
+    "url": "/static/js/2.0c28d90d.chunk.js"
+  },
+  {
+    "revision": "7fe6a12c7050af1fafd6",
+    "url": "/static/css/main.5e158310.chunk.css"
+  },
+  {
+    "revision": "8abacdbe68f6f075b4b5d030d2f0d115",
+    "url": "/index.html"
+  }
+];
