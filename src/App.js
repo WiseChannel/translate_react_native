a4ebcf1acb4d871dc3690e00f6495eec80403cb4
Wise Ilya <johndoe@example.com>
2019-04-13 14:36:50 +0300

    newPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-  
+  //
+  // class NewComp extends ReactDOM.Component {
+  //     // constructor(props) {
+  //     //
+  //     // },
+  //
+  //
+  //     listType(props) {
+  //         <div>
+  //             <form>
+  //                 <ul className={newList}>
+  //                     <li>#1</li>
+  //                     <li>#2</li>
+  //                     <li>#3</li>
+  //                     <li>#4</li>
+  //                 </ul>
+  //             </form>
+  //         </div>
+  //     }
+  //
+  //
+  //     render() {
+  //         return(
+  //           this.listType(props)
+  //         )
+  //     }
+  // }
+  //
+  //
+  // export default App.js;
