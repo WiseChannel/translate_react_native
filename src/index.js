8f94313efd553f76cdec1849351eb5ef1a4fccdd
Wise Ilya <johndoe@example.com>
2019-04-27 21:20:02 +0300

    new page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Redux from 'redux'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Redux from 'redux'
 // import './index.css';
 // import App from 'App';
 // import MyCom from 'MyCom'
